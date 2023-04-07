f95b879b137490873ee67c815c171a9c6dd93902
UmutBayraktaroglu <74791878+UmutBayraktaroglu@users.noreply.github.com>
2023-04-07 10:47:27 +0300

    Update Dictionary.java

--- project/src/main/java/com/example/project/Dictionary.java
+++ project/src/main/java/com/example/project/Dictionary.java
@@ -13,6 +13,30 @@ public class Dictionary {
     public static final String deu_eng = "src\\main\\java\\com\\example\\project\\Languages\\deu-eng.tei";
     public static final String deu_swe = "src\\main\\java\\com\\example\\project\\Languages\\deu-swe.tei";
     public static final String deu_tur = "src\\main\\java\\com\\example\\project\\Languages\\deu-tur.tei";
+    public static final String deu_ell = "src\\main\\java\\com\\example\\project\\Languages\\deu-ell.tei";
+    public static final String ell_eng = "src\\main\\resources\\com\\example\\project\\Language\\ell-eng.tei";
+    public static final String ell_fra = "src\\main\\resources\\com\\example\\project\\Language\\ell-fra.tei";
+    public static final String ell_ita = "src\\main\\resources\\com\\example\\project\\Language\\ell-ita.tei";
+    public static final String ell_swe = "src\\main\\resources\\com\\example\\project\\Language\\ell-swe.tei";
+    public static final String fra_deu = "src\\main\\resources\\com\\example\\project\\Language\\fra-deu.tei";
+    public static final String fra_ell = "src\\main\\resources\\com\\example\\project\\Language\\fra-ell.tei";
+    public static final String fra_eng = "src\\main\\resources\\com\\example\\project\\Language\\fra-eng.tei";
+    public static final String fra_ita = "src\\main\\resources\\com\\example\\project\\Language\\fra-ita.tei";
+    public static final String fra_swe = "src\\main\\resources\\com\\example\\project\\Language\\fra-swe.tei";
+    public static final String fra_tur = "src\\main\\resources\\com\\example\\project\\Language\\fra-tur.tei";
+    public static final String ita_deu = "src\\main\\resources\\com\\example\\project\\Language\\ita-deu.tei";
+    public static final String ita_ell = "src\\main\\resources\\com\\example\\project\\Language\\ita-ell.tei";
+    public static final String ita_eng = "src\\main\\resources\\com\\example\\project\\Language\\ita-eng.tei";
+    public static final String ita_swe = "src\\main\\resources\\com\\example\\project\\Language\\ita-swe.tei";
+    public static final String ita_tur = "src\\main\\resources\\com\\example\\project\\Language\\ita-tur.tei";
+    public static final String swe_deu = "src\\main\\resources\\com\\example\\project\\Language\\swe-deu.tei";
+    public static final String swe_ell = "src\\main\\resources\\com\\example\\project\\Language\\swe-ell.tei";
+    public static final String swe_eng = "src\\main\\resources\\com\\example\\project\\Language\\swe-eng.tei";
+    public static final String swe_fra = "src\\main\\resources\\com\\example\\project\\Language\\swe-fra.tei";
+    public static final String swe_ita = "src\\main\\resources\\com\\example\\project\\Language\\swe-ita.tei";
+    public static final String swe_tur = "src\\main\\resources\\com\\example\\project\\Language\\swe-tur.tei";
+    public static final String tur_deu = "src\\main\\resources\\com\\example\\project\\Language\\tur-deu.tei";
+    public static final String tur_eng = "src\\main\\resources\\com\\example\\project\\Language\\tur-eng.tei";
 
     public enum Language{ // to handle the differences of the xml files.
         ENGLISH,
