4938b780d16ca2e052753d9ee09f9072db5d5340
Hümay <456789hz@gmail.com>
2023-04-09 21:44:30 +0300

    Update Controller.java

--- project/src/main/java/com/example/project/Controller.java
+++ project/src/main/java/com/example/project/Controller.java
@@ -1,12 +1,15 @@
 package com.example.project;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class Controller {
 
@@ -47,73 +50,77 @@ public class Controller {
     private TextField turkishTrans;
 
     private Translator translator;
-    private String[] languages;
-    private TextField[] languages_tf;
+    private HashMap<String, TextField> languages_tf_map;
 
     public void initialize(){
         Translator translator = new Translator();
         this.translator = translator;
 
-        this.languages = new String[]{"Turkish", "English", "French", "German", "Italian", "Swedish", "Modern Greek"};
-        this.languages_tf = new TextField[]{turkishTrans, englishTrans, frenchTrans, germanTrans, italianTrans, swedishTrans, ellTrans};
+        this.languages_tf_map = new HashMap<>();
+        languages_tf_map.put("Turkish", turkishTrans);
+        languages_tf_map.put("English", englishTrans);
+        languages_tf_map.put("French", frenchTrans);
+        languages_tf_map.put("German", germanTrans);
+        languages_tf_map.put("Italian", italianTrans);
+        languages_tf_map.put("Swedish", swedishTrans);
+        languages_tf_map.put("Modern Greek", ellTrans);
     }
 
     @FXML
     public void exit1(ActionEvent event) {
         javafx.stage.Stage stage = (javafx.stage.Stage) exitButton.getScene().getWindow();
-        stage.close();  //exit
+        stage.close();
     }
 
     @FXML
     public void translatorEnter(ActionEvent event) {
 
-        String enteredword =  enterWordTextBox.getText();
+        String enteredword=  enterWordTextBox.getText();
+        enteredword = enteredword.toLowerCase();
         if(enteredword.equals("")){
-            javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.WARNING, "Please write and select the language of the word!", javafx.scene.control.ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Please write and select the language of the word!", ButtonType.OK);
             alert.showAndWait();
             enterWordTextBox.clear();
             return;
         }
         String source_lang = choicebox.getSelectionModel().getSelectedItem();
         if (source_lang == null) {
-            javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.WARNING, "Please select the language of the word!", javafx.scene.control.ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Please write and select the language of the word!", ButtonType.OK);
             alert.showAndWait();
             enterWordTextBox.clear();
             return;
         }
 
-        ArrayList<HashMap<String, ArrayList<HashMap<String, String>>>> x2all_lang_execution_list = null;
         HashMap<String, ArrayList<HashMap<String, String>>> xToy_translation_execution_map = null;
         ArrayList<HashMap<String, String>> xToy_list_of_executions = null;
 
-        HashMap<String, ArrayList<HashMap<String, ArrayList<HashMap<String, String>>>>> translator_map = this.translator.getTranslator();
+        HashMap<String, HashMap<String, ArrayList<HashMap<String, String>>>> translator_map = this.translator.getTranslator();
 
-        x2all_lang_execution_list = translator_map.get(source_lang);
-        for(int lang_index_i=0; lang_index_i<languages.length; lang_index_i++){
-            xToy_translation_execution_map = x2all_lang_execution_list.get(lang_index_i);
-
-            String target_lang = languages[lang_index_i];
-            TextField target_lang_tf = languages_tf[lang_index_i];
+        xToy_translation_execution_map = translator_map.get(source_lang);
+        for(Map.Entry<String, ArrayList<HashMap<String, String>>> entry : xToy_translation_execution_map.entrySet()) {
+            String target_lang = entry.getKey();
+            TextField target_lang_tf = this.languages_tf_map.get(target_lang);
+            xToy_list_of_executions = entry.getValue();
 
             String translated_text = enteredword;
-            System.out.printf("TARGET %s\n", target_lang);
-            xToy_list_of_executions = xToy_translation_execution_map.get(target_lang);
             if(xToy_list_of_executions!=null) {
                 for (int exec_index = 0; exec_index < xToy_list_of_executions.size(); exec_index++) {
+                    System.out.printf("%s", translated_text);
                     HashMap<String, String> dict = xToy_list_of_executions.get(exec_index);
                     if (dict.containsKey(translated_text)) {
                         translated_text = xToy_list_of_executions.get(exec_index).get(translated_text);
                     } else {
-                        translated_text = "";
+                        translated_text = "'NOT FOUND!'";
                         break;
                     }
                 }
+            }else{
+                System.out.printf("%s\n", target_lang);
             }
 
             target_lang_tf.setText(translated_text);
         }
 
-        enterWordTextBox.clear();
     }
 
 
