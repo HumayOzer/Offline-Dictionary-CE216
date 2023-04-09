2b403d5ff5319b7df26787dde0b4a6ea2b85abb3
Hümay <456789hz@gmail.com>
2023-04-09 18:56:10 +0300

    xmlparser class and translator class modified

--- project/src/main/java/com/example/project/XmlParser.java
+++ project/src/main/java/com/example/project/XmlParser.java
@@ -12,52 +12,80 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 public class XmlParser {
-    public HashMap<String,String> dictionaryParserXML(String path, Dictionary.Language currLanguage) {
+        public HashMap<String,String> dictionaryParserXML(String path, Dictionary.Language currLanguage){
 
-        HashMap<String,String> currDictionary = new HashMap<>();
+                HashMap<String,String> currDictionary = new HashMap<>();
 
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder;
-        Document doc;
-        File file = new File(path);
+                String extractEntryPatternString = "<entry(.*?)</entry>";
+                Pattern extractEntryPattern = Pattern.compile(extractEntryPatternString, Pattern.DOTALL);
 
-        try {
-            documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            doc = documentBuilder.parse(file);
-            doc.getDocumentElement().normalize();
-            NodeList entryNodeList;
-            entryNodeList = doc.getElementsByTagName("entry");
+                String extractSourceWordString = "<orth>(.*?)</orth>";
+                Pattern extractSourceWordPattern = Pattern.compile(extractSourceWordString, Pattern.DOTALL);
 
-            for (int i = 0; i < entryNodeList.getLength(); i++) {
-                Node entryNodes = entryNodeList.item(i);
-                if (entryNodes.getNodeType() == Node.ELEMENT_NODE){
-                    Element entryElements = (Element) entryNodes;
+                String extractTargetWordString1 = "<sense[^>]*>\\s*<cit[^>]*>\\s*<quote[^>]*>(.*?)</quote>";
+                Pattern extractTargetWordPattern1 = Pattern.compile(extractTargetWordString1, Pattern.DOTALL);
 
-                    String word = entryElements.getElementsByTagName("orth").item(0).getTextContent(); //word
-                    String definition;
+                String extractTargetWordString2 = "<def>(.*?)</def>";
+                Pattern extractTargetWordPattern2 = Pattern.compile(extractTargetWordString2, Pattern.DOTALL);
 
-                    if (currLanguage == Dictionary.Language.GREEK){
-                        definition = entryElements.getElementsByTagName("def").item(0).getTextContent();//definition
-                    }else {
-                        definition = entryElements.getElementsByTagName("quote").item(0).getTextContent();//definition
-                    }
-                    currDictionary.put(word,definition);
+                StringBuilder sb = new StringBuilder();
+                try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                                sb.append(line);
+                                sb.append(System.lineSeparator());
+                        }
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+
+                String fileContent = sb.toString();
+                Matcher matcher1 = extractEntryPattern.matcher(fileContent);
+                while (matcher1.find()) {
+                        String entryText = matcher1.group(1);
+
+                        String orth = "";
+                        String quote = "";
+
+                        Matcher matcher2 = extractSourceWordPattern.matcher(entryText);
+                        while (matcher2.find()) {
+                                orth = matcher2.group(1).trim();
+                                //System.out.println("Orth: " + orth);
+                        }
+
+                        Matcher matcher3 = extractTargetWordPattern1.matcher(entryText);
+                        while (matcher3.find()) {
+                                quote = matcher3.group(1).trim();
+                                //System.out.println("Quote: " + quote);
+                                break;
+                        }
+
+                        if (quote.equals("")) {
+                                Matcher matcher4 = extractTargetWordPattern2.matcher(entryText);
+                                while (matcher4.find()) {
+                                        quote = matcher4.group(1).trim();
+                                        //System.out.println("Quote: " + quote);
+                                        break;
+                                }
+                        }
+
+                        if (!orth.equals("") || !quote.equals("")) {
+                                currDictionary.put(orth, quote);
+                        }
 
                 }
 
-            }
 
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (SAXException e) {
-            throw new RuntimeException(e);
+                System.out.printf("File read: %s - %d\n", path, currDictionary.size());
+
+                return currDictionary;
         }
-
-
-        return currDictionary;
-    }
 }
