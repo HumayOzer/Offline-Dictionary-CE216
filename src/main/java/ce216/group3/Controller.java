aa3000ef595dcd17c1b6fdf6b55dd8a606fe5c9b
Hümay <456789hz@gmail.com>
2023-05-07 21:26:22 +0300

    final

--- src/main/java/ce216/group3/Controller.java
+++ src/main/java/ce216/group3/Controller.java
@@ -80,12 +80,25 @@ public class Controller {
         // Change Available Fields for Lang
         dicLangOptions.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
             dicFieldTur.clear();
+            dicFieldTur.setDisable(false);
+
             dicFieldEng.clear();
+            dicFieldEng.setDisable(false);
+
             dicFieldFra.clear();
+            dicFieldFra.setDisable(false);
+
             dicFieldDeu.clear();
+            dicFieldDeu.setDisable(false);
+
             dicFieldIta.clear();
+            dicFieldIta.setDisable(false);
+
             dicFieldSwe.clear();
+            dicFieldSwe.setDisable(false);
+
             dicFieldEll.clear();
+            dicFieldEll.setDisable(false);
         });
 
         // Change Available Fields for Lang
@@ -174,7 +187,16 @@ public class Controller {
             } else {
                 if ( execList != null ) {
                     for (HashMap<String, String> xToyListOfExecution : execList) {
-                        targetLang.setText(xToyListOfExecution.getOrDefault(dicWord, "Translation Doesn't Exist"));
+                        if (xToyListOfExecution.containsKey(dicWord)) {
+                            targetLang.setText(xToyListOfExecution.get(dicWord));
+
+                            String canBeEditableCheck = Helper.getLangCode(targetLangKey) + "_" + Helper.getLangCode(dicWordLang);
+                            targetLang.setDisable(Helper.canBeEditable(canBeEditableCheck));
+                        } else {
+                            targetLang.setText("Translation Doesn't Exist");
+                            targetLang.setDisable(true);
+                            break;
+                        }
                     }
                 }
             }
@@ -208,14 +230,21 @@ public class Controller {
 
             if ( execList != null ) {
                 for (HashMap<String, String> xToyListOfExecution : execList) {
+
                     if (xToyListOfExecution.containsKey(dicWord)) {
                         if (Objects.equals(dicWordLang, "TURKISH") && Objects.equals(targetLangKey, "ENGLISH")) {
                             // If dicWordLang is TURKISH and targetLangKey is ENGLISH send it to dicTurSearch()
+                            targetLang.setDisable(true);
                             dicTurSearch(xToyListOfExecution.get(dicWord));
+                        } else {
+                            String canBeEditableCheck = Helper.getLangCode(targetLangKey) + "_" + Helper.getLangCode(dicWordLang);
+                            targetLang.setDisable(Helper.canBeEditable(canBeEditableCheck));
                         }
+
                         targetLang.setText(xToyListOfExecution.get(dicWord));
                     } else {
                         targetLang.setText("Translation Doesn't Exist");
+                        targetLang.setDisable(true);
                         break;
                     }
                 }
@@ -249,8 +278,8 @@ public class Controller {
         }
 
         try {
-            // Turkish
             switch (dicWordLang) {
+                // Turkish
                 case "TURKISH" -> {
                     // Editing Files
                     new EditDictionary(DictionaryOptions.tur_eng, dicWord, dicTextFieldEng);
@@ -258,73 +287,51 @@ public class Controller {
                     // Fields will be edited from english
                     new EditDictionary(DictionaryOptions.eng_ita, dicTextFieldEng, dicTextFieldIta);
                     new EditDictionary(DictionaryOptions.eng_swe, dicTextFieldEng, dicTextFieldSwe);
-                    new EditDictionary(DictionaryOptions.eng_ell, dicTextFieldEng, dicTextFieldEll);
                     new EditDictionary(DictionaryOptions.eng_fra, dicTextFieldEng, dicTextFieldFra);
+                    // These options disabled due the file corruption problems
+                    // eng_ell
                 }
-
                 // German
                 case "GERMAN" -> {
-                    new EditDictionary(DictionaryOptions.deu_eng, dicWord, dicTextFieldEng);
                     new EditDictionary(DictionaryOptions.deu_tur, dicWord, dicTextFieldTur);
                     new EditDictionary(DictionaryOptions.deu_ita, dicWord, dicTextFieldIta);
-                    new EditDictionary(DictionaryOptions.deu_swe, dicWord, dicTextFieldSwe);
-                    new EditDictionary(DictionaryOptions.deu_ell, dicWord, dicTextFieldEll);
-                    new EditDictionary(DictionaryOptions.deu_fra, dicWord, dicTextFieldFra);
+                    // These options disabled due the file corruption problems
+                    // deu_eng, deu_swe, deu_ell, deu_fra
                 }
                 // Italian
                 case "ITALIAN" -> {
                     new EditDictionary(DictionaryOptions.ita_eng, dicWord, dicTextFieldEng);
-                    new EditDictionary(DictionaryOptions.ita_tur, dicWord, dicTextFieldTur);
                     new EditDictionary(DictionaryOptions.ita_deu, dicWord, dicTextFieldDeu);
-                    new EditDictionary(DictionaryOptions.ita_swe, dicWord, dicTextFieldSwe);
-                    new EditDictionary(DictionaryOptions.ita_ell, dicWord, dicTextFieldEll);
-                    new EditDictionary(DictionaryOptions.ita_fra, dicWord, dicTextFieldFra);
+                    // These options disabled due the file corruption problems
+                    // ita_swe, ita_ell, ita_fra, ita_tur
                 }
-
                 // Swedish
                 case "SWEDISH" -> {
                     new EditDictionary(DictionaryOptions.swe_eng, dicWord, dicTextFieldEng);
-                    new EditDictionary(DictionaryOptions.swe_tur, dicWord, dicTextFieldTur);
-                    new EditDictionary(DictionaryOptions.swe_ita, dicWord, dicTextFieldIta);
-                    new EditDictionary(DictionaryOptions.swe_deu, dicWord, dicTextFieldDeu);
-                    new EditDictionary(DictionaryOptions.swe_ell, dicWord, dicTextFieldEll);
-                    new EditDictionary(DictionaryOptions.swe_fra, dicWord, dicTextFieldFra);
+                    // These options disabled due the file corruption problems
+                    // swe_tur, swe_ita, swe_deu, swe_ell, swe_fra
                 }
-
                 // English
                 case "ENGLISH" -> {
-                    new EditDictionary(DictionaryOptions.eng_deu, dicWord, dicTextFieldDeu);
                     new EditDictionary(DictionaryOptions.eng_tur, dicWord, dicTextFieldTur);
                     new EditDictionary(DictionaryOptions.eng_ita, dicWord, dicTextFieldIta);
                     new EditDictionary(DictionaryOptions.eng_swe, dicWord, dicTextFieldSwe);
-                    new EditDictionary(DictionaryOptions.eng_ell, dicWord, dicTextFieldEll);
                     new EditDictionary(DictionaryOptions.eng_fra, dicWord, dicTextFieldFra);
+                    // These options disabled due the file corruption problems
+                    // eng_ell, eng_deu
                 }
-
                 // Modern greek
-                case "GREEK" -> {
-                    new EditDictionary(DictionaryOptions.ell_deu, dicWord, dicTextFieldDeu);
-                    new EditDictionary(DictionaryOptions.ell_tur, dicWord, dicTextFieldTur);
-                    new EditDictionary(DictionaryOptions.ell_ita, dicWord, dicTextFieldIta);
-                    new EditDictionary(DictionaryOptions.ell_swe, dicWord, dicTextFieldSwe);
-                    new EditDictionary(DictionaryOptions.ell_eng, dicWord, dicTextFieldEng);
-                    new EditDictionary(DictionaryOptions.ell_fra, dicWord, dicTextFieldFra);
+                case "MODERN_GREEK" -> {
+                    // These options disabled due the file corruption problems
+                    // Modern Greek cannot be edited.
                 }
-                //french
+                // French
                 case "FRENCH" -> {
-                    new EditDictionary(DictionaryOptions.fra_deu, dicWord, dicTextFieldDeu);
-                    new EditDictionary(DictionaryOptions.fra_tur, dicWord, dicTextFieldTur);
-                    new EditDictionary(DictionaryOptions.fra_ita, dicWord, dicTextFieldIta);
-                    new EditDictionary(DictionaryOptions.fra_swe, dicWord, dicTextFieldSwe);
-                    new EditDictionary(DictionaryOptions.fra_ell, dicWord, dicTextFieldEll);
                     new EditDictionary(DictionaryOptions.fra_eng, dicWord, dicTextFieldEng);
+                    // These options disabled due the file corruption problems
+                    // fra_deu, fra_tur, fra_ita, fra_swe, fra_ell
                 }
             }
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setHeaderText("Success");
-            alert.setContentText("TEI file edited successfully");
-            alert.showAndWait();
         } catch (Exception ex) {
             // Print the stack trace to console for debugging
             ex.printStackTrace();
@@ -334,6 +341,10 @@ public class Controller {
             alert.showAndWait();
         }
 
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText("Success");
+        alert.setContentText("TEI file edited successfully, please reload the application in order to see the updated word!");
+        alert.showAndWait();
     }
     @FXML
     public void addWord() {
--- src/main/java/ce216/group3/Helper.java
+++ src/main/java/ce216/group3/Helper.java
@@ -2,6 +2,8 @@ package ce216.group3;
 import ce216.group3.dictionary.DictionaryOptions;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class Helper {
     public static String getLangCode(String lang) {
@@ -39,5 +41,21 @@ public class Helper {
 
     public static String dictionaryPath(DictionaryOptions arg) {
         return "src/main/resources/translations/" + arg + ".tei";
+    }
+
+    public static Boolean canBeEditable(String arg) {
+        String[] cantEditable = {
+                "tur_eng",
+                "eng_ell", "eng_deu",
+                "ita_swe", "ita_ell", "ita_fra", "ita_tur",
+                "deu_eng", "deu_swe", "deu_ell", "deu_fra",
+                "fra_deu", "fra_tur", "fra_ita", "fra_swe", "fra_ell",
+                "swe_tur", "swe_ita", "swe_deu", "swe_ell", "swe_fra",
+                "ell_eng", "ell_deu", "ell_swe", "ell_ita", "ell_fra", "ell_tur"
+        };
+
+        List<String> cantEditableList = Arrays.asList(cantEditable);
+
+        return cantEditableList.contains(arg);
     }
 }
